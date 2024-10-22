89dba02b1e6e2ee862e48d6e5ba395903c8d75ca
SrDevel <david.villada@correo.tdea.edu.co>
2024-10-22 09:45:15 -0500

    Se agrega la librería para renderizar las fórmulas

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { InlineMath, BlockMath } from 'react-katex';
 
 interface MarkdownRendererProps {
     markdownText: string;
 }
 
-const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({markdownText}) => {
+const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ markdownText }) => {
+    // Función para renderizar matemáticas
+    const renderers = {
+        inlineMath: ({ value }: { value: string }) => <InlineMath math={value} />,
+        blockMath: ({ value }: { value: string }) => <BlockMath math={value} />
+    };
+
     return (
         <div className="markdown-body">
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdownText}</ReactMarkdown>
+            <ReactMarkdown
+                remarkPlugins={[remarkGfm]}
+                components={renderers}
+            >
+                {markdownText}
+            </ReactMarkdown>
         </div>
     );
 };
